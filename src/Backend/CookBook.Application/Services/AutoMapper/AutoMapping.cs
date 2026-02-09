dce49178b14ffbd2b8f78b3b24e056d2042fe057
ThIaGoOLuiZz <thiagoluizgon13@gmail.com>
2026-02-09 07:30:10 -0300

    new: adding AutoMapper

---
+++ src/Backend/CookBook.Application/Services/AutoMapper/AutoMapping.cs
@@ -0,0 +1,25 @@
+﻿
+using AutoMapper;
+using CookBook.Communication.Requests;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CookBook.Application.Services.AutoMapper
+{
+    internal class AutoMapping : Profile
+    {
+        public AutoMapping()
+        {
+            RequestToDomain();
+        }
+
+        private void RequestToDomain()
+        {
+            CreateMap<RequestRegisterUserJson, Domain.Entities.User>()
+                .ForMember(dest => dest.Password, option => option.Ignore());
+        }
+    }
+}
--- src/Backend/CookBook.Application/UseCases/User/Register/RegisterUserUseCase.cs
+++ src/Backend/CookBook.Application/UseCases/User/Register/RegisterUserUseCase.cs
@@ -1,6 +1,8 @@
-﻿using CookBook.Communication.Requests;
+﻿using CookBook.Application.Services.AutoMapper;
+using CookBook.Communication.Requests;
 using CookBook.Communication.Responses;
 using CookBook.Exceptions.ExceptionsBase;
+using Microsoft.Extensions.Logging.Abstractions;
 
 namespace CookBook.Application.UseCases.User.Register
 {
@@ -9,6 +11,13 @@ namespace CookBook.Application.UseCases.User.Register
         public ResponseRegisteredUserJson Execute(RequestRegisterUserJson request)
         {
             Validate(request);
+
+            var automapper = new AutoMapper.MapperConfiguration(options =>
+            {
+                options.AddProfile(new AutoMapping());
+            }, NullLoggerFactory.Instance).CreateMapper();
+
+            var user = automapper.Map<Domain.Entities.User>(request);
 
             return new ResponseRegisteredUserJson
             {
