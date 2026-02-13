48d3657290c6924d6e6a43cb5b47b8b8f5d311ae
ThIaGoOLuiZz <thiagoluizgon13@gmail.com>
2026-02-13 06:59:20 -0300

    new: adding DI

--- src/Backend/CookBook.Application/DependencyInjectionExtension.cs
+++ src/Backend/CookBook.Application/DependencyInjectionExtension.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using CookBook.Application.Services.AutoMapper;
+using CookBook.Application.Services.Cryptography;
+using CookBook.Application.UseCases.User.Register;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging.Abstractions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,7 +15,26 @@ namespace CookBook.Application
     {
         public static void AddApplication(this IServiceCollection services)
         {
+            AddAutoMapper(services);
+            AddUseCases(services);
+            AddPasswordEncrypter(services);
+        }
 
+        private static void AddAutoMapper(IServiceCollection services)
+        {
+            services.AddScoped(options => new AutoMapper.MapperConfiguration(options =>
+            {
+                options.AddProfile(new AutoMapping());
+            }, NullLoggerFactory.Instance).CreateMapper());
+        }
+
+        private static void AddUseCases(IServiceCollection services)
+        {
+            services.AddScoped<IRegisterUserUseCase, RegisterUserUseCase>();
+        }
+        private static void AddPasswordEncrypter(IServiceCollection services)
+        {
+            services.AddScoped(options => new PasswordEncripter());
         }
     }
 }
--- src/Backend/CookBook.Application/UseCases/User/Register/RegisterUserUseCase.cs
+++ src/Backend/CookBook.Application/UseCases/User/Register/RegisterUserUseCase.cs
@@ -1,4 +1,5 @@
-﻿using CookBook.Application.Services.AutoMapper;
+﻿using AutoMapper;
+using CookBook.Application.Services.AutoMapper;
 using CookBook.Application.Services.Cryptography;
 using CookBook.Communication.Requests;
 using CookBook.Communication.Responses;
@@ -8,23 +9,27 @@ using Microsoft.Extensions.Logging.Abstractions;
 
 namespace CookBook.Application.UseCases.User.Register
 {
-    public class RegisterUserUseCase
+    public class RegisterUserUseCase : IRegisterUserUseCase
     {
         private readonly IUserWriteOnlyRepository _writeOnlyRepository;
         private readonly IUserReadOnlyRepository _readOnlyRepository;
+        private readonly IMapper _automapper;
+        private readonly PasswordEncripter _passwordEncripter;
+
+        public RegisterUserUseCase(IUserWriteOnlyRepository writeOnlyRepository, IUserReadOnlyRepository readOnlyRepository, IMapper automapper, PasswordEncripter passwordEncripter)
+        {
+            _writeOnlyRepository = writeOnlyRepository;
+            _readOnlyRepository = readOnlyRepository;
+            _automapper = automapper;
+            _passwordEncripter = passwordEncripter;
+        }
 
         public async Task<ResponseRegisteredUserJson> Execute(RequestRegisterUserJson request)
         {
-            var criptografiaSenha = new PasswordEncripter();
-            var automapper = new AutoMapper.MapperConfiguration(options =>
-            {
-                options.AddProfile(new AutoMapping());
-            }, NullLoggerFactory.Instance).CreateMapper();
-
             Validate(request);
 
-            var user = automapper.Map<Domain.Entities.User>(request);
-            user.Password = criptografiaSenha.Encrypt(request.Password);
+            var user = _automapper.Map<Domain.Entities.User>(request);
+            user.Password = _passwordEncripter.Encrypt(request.Password);
 
             await _writeOnlyRepository.Add(user);
 
